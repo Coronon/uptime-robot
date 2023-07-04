1eb3c3f4cbc9702311f9f33b2ce599182f5ad7f5
Rubin Raithel <33808743+Coronon@users.noreply.github.com>
2023-07-04 19:02:51 +0200

    feat: add basic service shell

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/Coronon/uptime-robot
 
 go 1.20
+
+require github.com/kardianos/service v1.2.2
+
+require golang.org/x/sys v0.0.0-20201015000850-e3ed0017c211 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,52 @@
 package main
 
+import (
+	"log"
+	"time"
+
+	"github.com/kardianos/service"
+)
+
+const version = "v1.0.0"
+const serviceName = "de.rubinraithel.uptime-robot"
+const displayName = "Uptime-Robot" + " " + version
+const serviceDesc = "Utility service that provides push based uptime monitoring for various services"
+
+type program struct{}
+
+func (p program) Start(s service.Service) error {
+	log.Print(s.String() + " started")
+	go p.run()
+	return nil
+}
+
+func (p program) Stop(s service.Service) error {
+	log.Print(s.String() + " stopped")
+	return nil
+}
+
+func (p program) run() {
+	for {
+		log.Print("Service is running")
+		time.Sleep(1 * time.Second)
+	}
+}
+
 func main() {
-	
+	serviceConfig := &service.Config{
+		Name:        serviceName,
+		DisplayName: displayName,
+		Description: serviceDesc,
+	}
+
+	prg := &program{}
+	s, err := service.New(prg, serviceConfig)
+
+	if err != nil {
+		log.Fatal("Cannot create the service: " + err.Error())
+	}
+	err = s.Run()
+	if err != nil {
+		log.Fatal("Cannot start the service: " + err.Error())
+	}
 }
